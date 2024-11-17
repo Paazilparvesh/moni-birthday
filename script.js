40ea5731352a9fabb0600d58f37ca99643b32095
Paazilparvesh <152929689+Paazilparvesh@users.noreply.github.com>
2024-11-17 19:55:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('surpriseBtn').addEventListener('click', function() {
     let surprise = document.getElementById('extraSurprise');
-    surprise.innerHTML = '<p>🎉✨ SURPRISE!!! ✨🎉</p><p>May your life be filled with amazing adventures and joy! 🌟</p>';
+    surprise.innerHTML = '<p>🎉✨ happy<br>birthday<br>to you<br>Moni!!! ✨🎉</p><p>May your life be filled with amazing adventures and  Wishing you a day filled with joy, laughter, and endless surprise joy! 🌟</p>';
     surprise.style.opacity = 1;
     surprise.style.transform = 'translateY(0)';
     document.body.style.animation = 'backgroundAnimation 1s ease-in-out infinite'; // To make the background even more dynamic
